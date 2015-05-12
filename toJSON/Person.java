9ae808b0e15388aaf7ab82e00b7c9ecd803c07e3
Austin Meier <diveintotheword@att.net>
2015-05-12 07:44:44 -0500

    more changes

--- toJSON/Person.java
+++ toJSON/Person.java
@@ -13,21 +13,6 @@ public class Person
 		this.age = age;
 	}
 	
-	public String getFname()
-	{
-		return fname;
-	}
-	
-	public String getLname()
-	{
-		return lname;
-	}
-	
-	public int getAge()
-	{
-		return age;
-	}
-	
 	public String toJSON()
 	{
 		return "Person" + "(" + "\"" + this.fname + "\"" + ", " + "\"" + this.lname + "\"" + ", " + Integer.toString(age) + ")";
